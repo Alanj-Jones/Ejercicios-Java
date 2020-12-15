61489beaf0b673ddf1909cd2796c45c670a7b31d
Alan Jones <alanj.jones@comunidad.ub.edu.ar>
2020-12-15 16:25:07 -0300

    Ejercicio Contar vocales

---
+++ Count vowels/CheckVowels.java
@@ -0,0 +1,30 @@
+public class CheckVowels {
+    public static int getCount(String str) {
+      int vowelsCount = 0;
+      for(int i = 0; i<str.length(); i++){
+              switch(str.charAt(i)){
+                  case 'a':
+                      vowelsCount++;
+                      break;
+                  case 'e':
+                      vowelsCount++;
+                      break;
+                  case 'i':
+                      vowelsCount++;
+                      break;
+                  case 'o':
+                      vowelsCount++;
+                      break;
+                  case 'u':
+                      vowelsCount++;
+                      break;
+              }
+          }
+        return vowelsCount;
+    }
+    
+    public static void main(String args[]){
+        System.out.println(getCount("Cinco vocales")); // 5
+        System.out.println(getCount("hola")); // 2
+    }
+}
