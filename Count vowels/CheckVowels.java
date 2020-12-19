61845dbd6614998353b42ad4b44732d5efe32d94
Alan Jones <alanj.jones@comunidad.ub.edu.ar>
2020-12-18 22:50:05 -0300

    Minor imporvement

--- Count vowels/CheckVowels.java
+++ Count vowels/CheckVowels.java
@@ -1,6 +1,7 @@
 public class CheckVowels {
     public static int getCount(String str) {
       int vowelsCount = 0;
+      str.toLowerCase();
       for(int i = 0; i<str.length(); i++){
               switch(str.charAt(i)){
                   case 'a':
