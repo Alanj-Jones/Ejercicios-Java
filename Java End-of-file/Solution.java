1ed9da3123ea8cfa1f0b3a26a1ae9d5b6e099802
Alan Jones <alanj.jones@comunidad.ub.edu.ar>
2020-12-31 18:32:17 -0300

    Subida del ejercicio End of File Corregida

--- Java End-of-file/Solution.java
+++ Java End-of-file/Solution.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class Solution {
 
     public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        
         Scanner in = new Scanner(System.in);
         int iteration = 1;
         while(true){            
