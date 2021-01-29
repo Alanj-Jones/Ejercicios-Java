b6a0e7d0f3ba58ea2bd4ec6c817c040b0e2178b3
Alan Jones <alanj.jones@comunidad.ub.edu.ar>
2021-01-29 01:48:47 -0300

    Java map

---
+++ Java Map/Solution.java
@@ -0,0 +1,28 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Solution {    
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+		int n=in.nextInt();
+        in.nextLine();
+        HashMap<String,Integer> agenda = new HashMap<>();
+
+		for(int i=0;i<n;i++) { 
+			String name = in.nextLine();
+			int phone = in.nextInt();
+            in.nextLine();
+            agenda.put(name, phone);
+		}
+		while(in.hasNext()) {
+            String s=in.nextLine();
+            if(agenda.containsKey(s)) {
+                System.out.println(s + '=' + agenda.get(s));
+            }else {
+                System.out.println("Not Found");                
+            }
+        }
+        in.close();
+    }
+}
