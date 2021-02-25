9c91f5dfbafc64c173be0afe3e8b88314a731812
Alan Jones <alanj.jones@comunidad.ub.edu.ar>
2021-02-25 20:30:06 -0300

    Java arraylist

---
+++ Java ArrayList/ArrayListExercise.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ArrayListExercise {
+ 
+    public static void main(String[] args) {
+        ArrayList<ArrayList<Integer>> multiple = new ArrayList<>(); 
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        for (int i=0; i<n; i++) {
+            ArrayList<Integer> single = new ArrayList<>();
+            int quantity = in.nextInt();
+            for (int j=0; j<quantity; j++) {
+                
+            }
+        }
+
+
+
+
+
+        in.close();
+
+    }
+}
