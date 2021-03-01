62cf9252b4b0dbf1295c22e323afc97da08f0385
Alan Jones <alanj.jones@comunidad.ub.edu.ar>
2021-02-28 21:12:36 -0300

    Array 1D

--- Java 1D Array (Part 2)/Solution.java
+++ Java 1D Array (Part 2)/Solution.java
@@ -3,7 +3,31 @@ import java.util.*;
 public class Solution {
 
     public static boolean canWin(int leap, int[] game) {
-        // Return true if you can win the game; otherwise, return false.
+        int gLength = game.length - 1;
+        int currentPosition = 0;
+
+        for (int i = 0; i < game.length; i++) {
+            int tempPos = currentPosition;
+            if (currentPosition >= gLength ){
+                return true;
+            }
+
+            if (currentPosition + leap > gLength) {
+                currentPosition += leap;
+            } else if (currentPosition + leap <= gLength && game[currentPosition + leap] != 1) {
+                    currentPosition += leap;
+            } else if (game[currentPosition + 1] != 1) {
+                currentPosition++;
+            } else if (currentPosition != 0 && game[currentPosition - 1] != 1) {
+                currentPosition--;
+            }
+
+            // System.out.println(currentPosition);
+            if (tempPos == currentPosition) {
+                return false;
+            }
+        }
+        return false;
     }
 
     public static void main(String[] args) {
